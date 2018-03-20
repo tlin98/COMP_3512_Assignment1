aafcd1b0fbd84526d26fcc663e188c201b4f1f90
tlin98 <tonylin514@gmail.com>
2018-03-20 13:26:48 -0700

    Commenting and generating "importance"

--- Assignment 1/Assignment 1/PageRank.cpp
+++ Assignment 1/Assignment 1/PageRank.cpp
@@ -1,8 +1,8 @@
 #include "PageRank.h"
 
 int main() {
-	
-	std::ifstream input_file("test.txt");
+	std::string filename = "test.txt";
+	std::ifstream input_file(filename);
 	std::vector<int> input;
 	int temp = 0, size = 0;
 	while (input_file >> temp) {
@@ -10,23 +10,48 @@ int main() {
 		++size;
 	}
 	int dimension = size / 4;
-	
 
-	//int input[16] = { 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0 };
-	//int input[4][4] = { {0, 1, 1, 0}, {1, 0, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0} };
-
+	// Add the values read from filename to 2D vector array matrix
 	std::vector<std::vector<double>> matrix;
+	std::cout << "Reading file " << filename << std::endl;
 	for (int y = 0; y < dimension; ++y) {
 		matrix.push_back(std::vector<double>());
 		for (int x = 0; x < dimension; ++x) {
 			matrix.at(y).push_back(input.at(y * dimension + x));
-			std::cout << matrix.at(y).at(x) << " ";
 		}
-		std::cout << std::endl;
 	}
+	printMatrix(matrix);
+
+	// Divide the values in the column by the sum of the column. 
+	// Generate "importance"
+	for (int x = 0; x < dimension; ++x) {
+		int sum = 0;
+		for (int y = 0; y < dimension; ++y) {
+			sum += (int) matrix.at(y).at(x);
+		}
+		for (int y = 0; y < dimension; ++y) {
+			if (matrix.at(y).at(x) != 0) {
+				matrix.at(y).at(x) = (double)matrix.at(y).at(x) / sum;
+			}
+		}
+	}
+	printMatrix(matrix);
 
 	system("pause");
 
 	return 0;
 	
+}
+
+void printMatrix(const std::vector<std::vector<double>> &arr) {
+	for (size_t y = 0; y < arr.size(); ++y) {
+		for (size_t x = 0; x < arr.size(); ++x) {
+			if (arr.at(y).at(x) != 0) {
+				std::cout << arr.at(y).at(x) << " ";
+			} else {
+				std::cout << arr.at(y).at(x) << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
 }
--- Assignment 1/Assignment 1/PageRank.h
+++ Assignment 1/Assignment 1/PageRank.h
@@ -3,4 +3,7 @@
 #include <array>
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <stdlib.h>
+
+void printMatrix(const std::vector<std::vector<double>> &arr);
